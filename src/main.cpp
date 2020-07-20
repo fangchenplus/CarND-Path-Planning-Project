bc0fdfe642fd8539f9893a019292e2a3deead8e1
fangchenplus <fangchenplus@gmail.com>
2020-07-19 20:24:04 -0700

    first commit

--- src/main.cpp
+++ src/main.cpp
@@ -7,11 +7,14 @@
 #include "Eigen-3.3/Eigen/QR"
 #include "helpers.h"
 #include "json.hpp"
+#include "spline.h"
 
 // for convenience
 using nlohmann::json;
 using std::string;
 using std::vector;
+using std::cout;
+using std::endl;
 
 int main() {
   uWS::Hub h;
@@ -97,7 +100,106 @@ int main() {
            * TODO: define a path made up of (x,y) points that the car will visit
            *   sequentially every .02 seconds
            */
+          // 4m lane width 
+          // The car does not exceed a total acceleration of 10 m/s^2 and a jerk of 10 m/s^3.
+          int lane = 1; // three lanes labeled as 0, 1, and 2
+          double ref_vel = 49.5; //mph
+          
+          vector<double> ptsx; //sparsed (x,y) points with 30m interval for interpolation
+          vector<double> ptsy;
+          
+          //set the car coordicate reference point as where the car is or at the previous paths end point
+          double ref_x = car_x; 
+          double ref_y = car_y;
+          double ref_yaw = deg2rad(car_yaw);
+                             
+          int prev_size = previous_path_x.size();
+          cout << "prev_size" << prev_size << endl;
+          
+          if (prev_size < 2) {
+            double prev_car_x = car_x - cos(car_yaw);
+            double prev_car_y = car_y - sin(car_yaw);
+            
+            ptsx.push_back(prev_car_x);
+            ptsx.push_back(car_x);
+            ptsy.push_back(prev_car_y);
+            ptsy.push_back(car_y);
+            
+          } else {
+            //redefine reference state as previous path end point
+            ref_x = previous_path_x[prev_size-1];
+            ref_y = previous_path_y[prev_size-1];
+            
+            double ref_x_prev = previous_path_x[prev_size-2];
+            double ref_y_prev = previous_path_y[prev_size-2];
+            ref_yaw = atan2(ref_y-ref_y_prev, ref_x-ref_x_prev);
+            
+            ptsx.push_back(ref_x_prev);
+            ptsx.push_back(ref_x);   
+            ptsy.push_back(ref_y_prev);
+            ptsy.push_back(ref_y); 
+            
+          }          
+          
+          
+          vector<double> next_wp0 = getXY(car_s+30, 2+4*lane, map_waypoints_s, map_waypoints_x, map_waypoints_y);
+          vector<double> next_wp1 = getXY(car_s+60, 2+4*lane, map_waypoints_s, map_waypoints_x, map_waypoints_y);
+          vector<double> next_wp2 = getXY(car_s+90, 2+4*lane, map_waypoints_s, map_waypoints_x, map_waypoints_y);
+          
+          ptsx.push_back(next_wp0[0]);
+          ptsx.push_back(next_wp1[0]);
+          ptsx.push_back(next_wp2[0]);
+          
+          ptsy.push_back(next_wp0[1]);
+          ptsy.push_back(next_wp1[1]);
+          ptsy.push_back(next_wp2[1]);   
 
+          // change from map to vechile coordicates
+          for (int i = 0; i < ptsx.size(); i++) {
+            double shift_x = ptsx[i] - ref_x;
+            double shift_y = ptsy[i] - ref_y;
+            
+            ptsx[i] = shift_x*cos(0.0-ref_yaw) - shift_y*sin(0.0-ref_yaw);
+            ptsy[i] = shift_x*sin(0.0-ref_yaw) + shift_y*cos(0.0-ref_yaw);
+          }
+          
+          // break up with spline points to travel at desired car velocity          
+          tk::spline s;
+          s.set_points(ptsx,ptsy);
+          
+          double target_x = 30.0;
+          double target_y = s(target_x);
+          double target_dist = sqrt(target_x*target_x + target_y*target_y);
+          double x_add_on = 0.0;
+          
+          for (int i = 0; i < prev_size; ++i) {
+            next_x_vals.push_back(previous_path_x[i]);
+            next_y_vals.push_back(previous_path_y[i]);
+          }
+
+
+          for (int i = 0; i < 50-prev_size; ++i) {    
+
+            // pos_x += (dist_inc)*cos(angle+(i+1)*(pi()/100));
+            // pos_y += (dist_inc)*sin(angle+(i+1)*(pi()/100));
+            double N = target_dist/(0.02*ref_vel/2.24);
+            double x_point = x_add_on + target_x/N;
+            double y_point = s(x_point);
+            
+            x_add_on = x_point; //x interval
+            
+            double x_ref = x_point;
+            double y_ref = y_point;
+            
+            // rotate back to map coordicates from vehicle coordinates
+            x_point = ref_x + (x_ref*cos(ref_yaw) - y_ref*sin(ref_yaw));
+            y_point = ref_y + (x_ref*sin(ref_yaw) + y_ref*cos(ref_yaw));
+            
+            next_x_vals.push_back(x_point);
+            next_y_vals.push_back(y_point);            
+          }
+
+          // end
 
           msgJson["next_x"] = next_x_vals;
           msgJson["next_y"] = next_y_vals;
